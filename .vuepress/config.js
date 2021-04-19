16614bdef810c0c033c3140a2f774258b4002901
秃尾巴的猫 <472647301@qq.com>
2021-04-19 19:50:57 +0800

    init

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: "Byron",
-  description: "xxx",
+  title: "秃尾巴的猫",
+  description: "",
   dest: "public",
   head: [
     [
@@ -20,82 +20,22 @@ module.exports = {
   ],
   theme: "reco",
   themeConfig: {
-    nav: [
-      {
-        text: "Home",
-        link: "/",
-        icon: "reco-home",
-      },
-      {
-        text: "TimeLine",
-        link: "/timeline/",
-        icon: "reco-date",
-      },
-      {
-        text: "Docs",
-        icon: "reco-message",
-        items: [
-          {
-            text: "vuepress-reco",
-            link: "/docs/theme-reco/",
-          },
-        ],
-      },
-      {
-        text: "Contact",
-        icon: "reco-message",
-        items: [
-          {
-            text: "GitHub",
-            link: "https://github.com/recoluan",
-            icon: "reco-github",
-          },
-        ],
-      },
-    ],
-    sidebar: {
-      "/docs/theme-reco/": ["", "theme", "plugin", "api"],
-    },
     type: "blog",
-    blogConfig: {
-      category: {
-        location: 2,
-        text: "Category",
-      },
-      tag: {
-        location: 3,
-        text: "Tag",
-      },
-    },
-    friendLink: [
-      {
-        title: "午后南杂",
-        desc: "Enjoy when you can, and endure when you must.",
-        email: "1156743527@qq.com",
-        link: "https://www.recoluan.com",
-      },
-      {
-        title: "vuepress-theme-reco",
-        desc: "A simple and beautiful vuepress Blog & Doc theme.",
-        avatar:
-          "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        link: "https://vuepress-theme-reco.recoluan.com",
-      },
-    ],
-    logo: "/logo.png",
     search: true,
     searchMaxSuggestions: 10,
     lastUpdated: "Last Updated",
-    author: "Byron",
-    authorAvatar: "/avatar.png",
-    startYear: "2020",
-    // 备案
-    record: "粤ICP备16063083号-1",
+    author: "秃尾巴的猫",
+    authorAvatar: "/avatar.jpeg",
+    record: "湘ICP备2021006437号-1",
     recordLink: "https://beian.miit.gov.cn",
-    // cyberSecurityRecord: '公安部备案文案',
-    // cyberSecurityLink: '公安部备案指向链接',
-    // 项目开始时间，只填写年份
-    startYear: "2020",
+    nav: [
+      { text: "TimeLine", link: "/timeline/", icon: "reco-date" },
+      {
+        text: "GitHub",
+        link: "https://github.com/472647301",
+        icon: "reco-github",
+      },
+    ],
   },
   markdown: {
     lineNumbers: true,
